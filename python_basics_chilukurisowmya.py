097064531652c600a89dfb085390f0404c2944c4
Chilukuri Sowmya <110710651+2100032530@users.noreply.github.com>
2026-04-05 14:48:37 +0530

    Add files via upload

---
+++ python_basics_chilukurisowmya.py
@@ -0,0 +1,22 @@
+Task 1:
+person_name=input("Enter Person name: ")
+person_age=int(input("Enter Person age: "))
+person_height=float(input("Enter Person height: "))
+is_person_student=boolean(input("Enter person status: "))
+
+print(person_name , person_age , person_height , is_person_student)
+
+Task 2:
+print(f"Name: {person_name} | Age: {person_age} | Height: {person_height} | Student: {is_person_student}")
+
+Task 3:
+age_in_months = age*12
+age_in_days = age*365
+remainder = age%7
+age_powerof2 = (age**2)
+
+print(age_in_months)
+print(age_in_days)
+print(remainder)
+print(age_powerof2)
+
